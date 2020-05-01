83da8e1a288abb3617569e38365c0c1bffe5c8a9
Den <39949334+shlvd@users.noreply.github.com>
2020-05-01 22:21:06 +0300

    Update nelder_mead_2d.py

--- nelder_mead_2d.py
+++ nelder_mead_2d.py
@@ -37,7 +37,6 @@ def f(point):
 
 def nelder_mead(alpha=1, beta=0.5, gamma=2, maxiter=10):
     
-    # initialization
     v1 = Vector(0, 0)
     v2 = Vector(1.0, 0)
     v3 = Vector(0, 1)
@@ -53,7 +52,6 @@ def nelder_mead(alpha=1, beta=0.5, gamma=2, maxiter=10):
         
         mid = (g + b)/2
 
-        # reflection
         xr = mid + alpha * (mid - w)
         if f(xr.c()) < f(g.c()):
             w = xr
@@ -65,7 +63,6 @@ def nelder_mead(alpha=1, beta=0.5, gamma=2, maxiter=10):
                 w = c
         if f(xr.c()) < f(b.c()):
 
-            # expansion
             xe = mid + gamma * (xr - mid)
             if f(xe.c()) < f(xr.c()):
                 w = xe
@@ -73,17 +70,14 @@ def nelder_mead(alpha=1, beta=0.5, gamma=2, maxiter=10):
                 w = xr
         if f(xr.c()) > f(g.c()):
             
-            # contraction
             xc = mid + beta * (w - mid)
             if f(xc.c()) < f(w.c()):
                 w = xc
 
-        # update points
         v1 = w
         v2 = g
         v3 = b
     return b
 
-print("Result of Nelder-Mead algorithm: ")
 xk = nelder_mead()
 print("Best poits is: %s"%(xk))
